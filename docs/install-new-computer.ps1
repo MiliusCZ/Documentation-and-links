c78c544c863562fea409c8b7180d93eb765f9217
MiliusCZ <milos.turek@gmail.com>
2016-02-05 22:07:35 +0100

    Create install-new-computer.ps1

---
+++ docs/install-new-computer.ps1
@@ -0,0 +1,38 @@
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Productivity
+
+#choco install chrome -y
+#choco install totalcommander -y 
+#choco install 7zip -y
+#choco install jdownloader -y
+#choco install googledrive -y
+#choco install office365proplus -y
+
+# Media
+
+#choco install foobar2000 -y
+#choco install reaper -y
+#choco install mpc-hc -y
+#choco install adobe-creative-cloud -y          
+#choco install spotify -y
+#choco install calibre -y
+#choco install picard -y
+#choco install plexmediaserver -y
+#choco install handbrake -y
+
+# Development
+
+#choco install nodejs -y
+#choco install git -y
+#choco install github -y
+#choco install sourcetree -y
+
+#choco install visualstudio2015community -y
+#choco install visualstudiocode -y
+#choco install fiddler -y
+
+#choco install virtualbox -y
+
+#choco install androidstudio -y
+#choco install arduino -y
